f3c1e10cb1c19abe7bc241569d16141a9c654e16
little_crabby <ivan.pangolin@gmail.com>
2019-11-13 14:23:49 +0200

    Settings model created

---
+++ Microting.InstallationCheckingBase/Infrastructure/Models/InstallationCheckingBaseSettings.cs
@@ -0,0 +1,11 @@
+namespace Microting.InstallationCheckingBase.Infrastructure.Models
+{
+    public class InstallationCheckingBaseSettings
+    {
+        public string MaxNumberOfWorkers { get; set; }
+        public string MaxParallelism { get; set; }
+        public string SdkConnectionString { get; set; }
+        public string InstallationFormId { get; set; }
+        public string RemovalFormId { get; set; }
+    }
+}
